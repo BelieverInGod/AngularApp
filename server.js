9bcb10e568d4a000ab8a3d5637b6603df9659a66
BelieverInGod <Stepanko919@gmail.com>
2023-01-27 16:48:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-app//index.html'));});
+'/dist/angular-app/index.html'));});
 app.listen(process.env.PORT || 8080);
